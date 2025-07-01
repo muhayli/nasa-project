115fa70bcaa479b7eba6a40c7d7af280d94c0f78
mohiden <mohidendj@gmail.com>
2025-07-01 14:05:27 +0100

    add live url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5050',
+        target: 'https://nasa-project-cggf.onrender.com/',
         changeOrigin: true,
       },
     },
